0ed6d892e45abc6c6fcff96923c60c08e2501314
Robin Steinhauser <rbn2208@gmail.com>
2021-03-01 13:58:51 +0100

    I dont know

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const hello = require('./hello')
 
 hello()
 
-const random = require('./random')
-// console.log(process.argv[2])
-const max = Number(process.argv[2])
-console.log(random(max))
+
+const read = require('./read.js')
+read('random')
---
+++ read.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+
+module.exports = function read(path){
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+        if(err) {
+            console.log('ERROR', err)
+        }else {
+            console.log(data.split('\n').length)
+        }
+    })
+}
